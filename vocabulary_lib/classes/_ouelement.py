997c0c0431d0d2c871efd8054a797fe90319aeea
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-10-05 14:47:49 +0200

    _ouelement and its tests concluded

--- vocabulary_lib/classes/_ouelement.py
+++ vocabulary_lib/classes/_ouelement.py
@@ -1,3 +1,14 @@
+"""
+This module provides the foundational class for representing elements in OntoUML models.
+It includes the `_OUElement` class, which serves as the main class for OntoUML models, and
+is designed to be private to prevent direct instantiation. The module also includes
+necessary exceptions for handling issues related to OntoUML ID and type mismatches.
+
+The `_OUElement` class is initialized with a URI reference representing the object's ID,
+and performs checks to ensure that the ID exists in the provided OntoUML graph and has a
+valid type. It raises exceptions (`OUIDNotInGraph`, `OUIDTypeMismatchError`) when these
+checks fail, providing informative error messages to facilitate debugging.
+"""
 from rdflib import URIRef, Graph
 
 from vocabulary_lib.classes.ouexception import OUIDNotInGraph, OUIDTypeMismatchError
@@ -9,14 +20,49 @@ class _OUElement:
     :param object_id: The URI reference of the cardinality object.
     :type object_id: URIRef
 
+    :ivar id: The URI reference of the object.
     :vartype id: URIRef
+    :ivar type: The type of the object as a URI reference.
+    :vartype type: URIRef
     """
 
     def __init__(self, ontouml_graph: Graph, object_id: URIRef, class_name: str, related_type: URIRef):
+        """
+        Initializes an instance of the _OUElement class, verifying the existence and type of the object in the OntoUML graph.
+
+        This constructor method performs two main checks on the provided object_id:
+        1. Checks if the object_id exists in the ontouml_graph.
+        2. Checks if the object_id has the expected related_type in the ontouml_graph.
+
+        If either check fails, an exception is raised. Otherwise, the instance is successfully created with the
+        specified object_id and related_type.
+
+        :param ontouml_graph: The OntoUML graph in which to look for the object_id.
+        :type ontouml_graph: Graph
+        :param object_id: The URI reference of the object being created.
+        :type object_id: URIRef
+        :param class_name: The name of the class for which the object is being created.
+        :type class_name: str
+        :param related_type: The expected type of the object in the OntoUML graph.
+        :type related_type: URIRef
+
+        :raises OUIDNotInGraph: If the object_id does not exist in the ontouml_graph.
+        :raises OUIDTypeMismatchError: If the object_id exists but does not have the expected related_type in the ontouml_graph.
+
+        :ivar id: The URI reference of the object, initialized to `object_id`.
+        :vartype id: URIRef
+        :ivar type: The type of the object as a URI reference, initialized to `related_type`.
+        :vartype type: URIRef
+        """
+        # Verify if ID exists in the graph
         if (object_id, None, None) not in ontouml_graph:
             raise OUIDNotInGraph(object_id, class_name)
+
+        # Verify if ID has a valid type
         elif (object_id, None, related_type) not in ontouml_graph:
             raise OUIDTypeMismatchError(object_id, class_name, related_type)
+
+        # If valid, instantiate
         else:
             self.id: URIRef = object_id
             self.type: URIRef = related_type
--- vocabulary_lib/tests/test_ouelement.py
+++ vocabulary_lib/tests/test_ouelement.py
@@ -1,23 +1,77 @@
-# OU Specific instantiated correctly
-# OU Specific instantiated wrongly must generate error
-# Check if OU Specific is instance of OUElement
-from rdflib import Graph
+"""This module contains unit tests for the _OUElement class.
+
+Each function in this module tests a different aspect of the _OUElement class, including positive cases,
+negative cases, and error handling.
+"""
+from rdflib import Graph, RDF, URIRef
 
 from vocabulary_lib.classes._ouelement import _OUElement
-from vocabulary_lib.classes.ouelement_types import OUClass
+from vocabulary_lib.classes.ouexception import OUIDNotInGraph, OUIDTypeMismatchError
+from vocabulary_lib.classes.outerm import OUTerm
 
 
-def test_ou_element_positive():
+def test_ou_element_creation_positive() -> None:
+    """Test the successful creation of an _OUElement instance.
+
+    This function tests the creation of an _OUElement instance with valid arguments, and checks that the id and
+    type attributes are set correctly.
+    """
     onto_graph = Graph()
-    elem_uri = "https://example.org/my_elem"
-    elem_obj = _OUElement(onto_graph, elem_uri)
-    assert str(elem_obj.id) == elem_uri
+    elem_uri = URIRef("https://example.org/my_elem")
+    elem_type = OUTerm.Class
+    onto_graph.add((elem_uri, RDF.type, elem_type))
+
+    elem_obj = _OUElement(onto_graph, elem_uri, "OUElement", elem_type)
+    assert elem_obj.id == elem_uri
+    assert elem_obj.type == elem_type
 
 
-def test_ou_element_negative():
-    elem_uri = "https://example.org/my_elem"
+def test_ou_element_negative_attribute() -> None:
+    """Test the handling of an attribute error.
+
+    This function tests that an AttributeError is raised when trying to access a non-existent attribute of
+    an _OUElement instance.
+    """
+    onto_graph = Graph()
+    elem_uri = URIRef("https://example.org/my_elem")
+    elem_type = OUTerm.Class
+    onto_graph.add((elem_uri, RDF.type, elem_type))
+    elem_obj = _OUElement(onto_graph, elem_uri, "OUElement", elem_type)
+
     try:
-        elem_obj = _OUElement(elem_uri)
-        elem_obj.non_existent_att
-    except Exception:
+        elem_obj.non_existent_attribute
+    except AttributeError:
+        assert True
+
+
+def test_ou_element_id_not_in_graph() -> None:
+    """Test the exception handling for a non-existent element ID.
+
+    This function tests that an OUIDNotInGraph exception is raised when trying to create an _OUElement instance with
+    an ID that is not present in the graph.
+    """
+    onto_graph = Graph()
+    elem_uri = URIRef("https://example.org/my_elem")
+    try:
+        elem_obj = _OUElement(onto_graph, elem_uri, "OUElement", "")
+        assert False
+    except OUIDNotInGraph:
+        assert True
+
+
+def test_ou_element_id_type_mismatch_error() -> None:
+    """Test the exception handling for a type mismatch error.
+
+    This function tests that an OUIDTypeMismatchError exception is raised when trying to create an _OUElement instance
+    with an ID whose type does not match the expected type.
+    """
+    onto_graph = Graph()
+    elem_uri = URIRef("https://example.org/my_elem")
+    elem_type = URIRef("my_type")
+    onto_graph.add((elem_uri, RDF.type, elem_type))
+
+    try:
+        elem_obj = _OUElement(onto_graph, elem_uri, "OUElement", OUTerm.Class)
+        assert False
+    except OUIDTypeMismatchError:
         assert True
