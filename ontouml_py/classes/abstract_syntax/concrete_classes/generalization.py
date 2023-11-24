0e958f9ac924789ea4347e08c0c783bb7562d2c5
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-11-24 10:39:24 +0100

    minor syntactical improvements

--- ontouml_py/classes/abstract_syntax/concrete_classes/generalization.py
+++ ontouml_py/classes/abstract_syntax/concrete_classes/generalization.py
@@ -1,4 +1,6 @@
-from ontouml_py.classes.abstract_syntax.abstract_classes.namedelement import NamedElement
+from ontouml_py.classes.abstract_syntax.abstract_classes.namedelement import (
+    NamedElement,
+)
 
 
 class Generalization(NamedElement):
