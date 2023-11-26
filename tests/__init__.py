373196ac0ee24aa43635eb7f4fe1cfb7aa621564
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-11-26 22:20:05 +0100

    added tests subpackage documentation

---
+++ tests/__init__.py
@@ -0,0 +1,46 @@
+"""
+Tests Subpackage for OntoUML Python Project
+============================================
+
+This subpackage contains a comprehensive suite of tests designed to ensure the robustness and correctness of the \
+OntoUML Python Project.
+It includes unit tests, integration tests, and other test utilities that collectively validate the functionality \
+of the project's components, including classes, methods, and integration points.
+
+Each test module within this subpackage is dedicated to a specific aspect of the project, ensuring thorough coverage \
+and validation of the project's features and behaviors. The tests are crafted to be as informative and communicative \
+as possible, adhering to best practices in software testing.
+
+Features
+--------
+- Comprehensive testing: Each class and method in the project is accompanied by a set of tests to validate its \
+expected behavior.
+- Informative assertions: Tests are designed with clear and informative assertion messages to aid in debugging \
+and understanding test failures.
+- Type hints and docstrings: Tests are annotated with type hints and detailed docstrings in Sphinx format, \
+providing clarity on test purposes and methodologies.
+- Fixture reuse: Wherever possible, fixtures are reused across tests to maintain consistency and reduce redundancy.
+- Error handling validation: Tests include scenarios to validate proper error handling and edge cases.
+
+Usage
+-----
+To run the tests, navigate to the project's root directory and execute the test suite using a test runner like pytest.
+
+.. code-block:: bash
+
+    pytest tests/
+
+This will run all tests in the `tests` subpackage and report the results.
+
+.. note::
+    The tests are designed to be run in an isolated environment to avoid interference with production data or \
+    configurations.
+
+.. warning::
+    Ensure all dependencies are installed and the Python environment is correctly set up before running the tests.
+
+Contributing
+------------
+Contributions to the test suite are welcome. When adding new tests or modifying existing ones, ensure they adhere to the
+project's testing standards and guidelines.
+"""
