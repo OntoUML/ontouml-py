182f443743cbeb87b5e27c7ba608020c6594321a
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-12-12 17:16:35 +0100

    pydantic removed from ontoumlelement.py and namedelement.py

--- ontouml_py/model/namedelement.py
+++ ontouml_py/model/namedelement.py
@@ -1,88 +1,18 @@
-"""Module for the abstract NamedElement class within an OntoUML model.
-
-This module defines the NamedElement class, an abstract class representing elements with names in an OntoUML model. It
-extends the OntoumlElement class and includes additional attributes and validation for managing named elements' details
-such as their preferred name, alternative names, descriptions, editorial notes, as well as lists of creators and
-contributors.
-"""
-from abc import abstractmethod
-from typing import Any
+from dataclasses import dataclass, field
 from typing import Optional
 
+from icecream import ic
 from langstring import LangString
-from pydantic import Field
-from pydantic import field_validator
-from pydantic_core.core_schema import ValidationInfo
 
 from ontouml_py.model.ontoumlelement import OntoumlElement
-from ontouml_py.utils.error_message import format_error_message
 
 
+@dataclass
 class NamedElement(OntoumlElement):
-    """An abstract class representing a named element within an OntoUML model, extending the OntoumlElement class.
 
-    This class provides functionality for managing named elements, including their preferred name, alternative names,
-    descriptions, editorial notes, as well as lists of creators and contributors.
-
-    :ivar names: The preferred names of the element, represented as a list of LangStrings.
-    :vartype names: list[LangString]
-    :ivar alt_names: A list of alternative names for the element, each represented as a LangString object.
-    :vartype alt_names: list[LangString]
-    :ivar description: A LangString object representing the description of the element.
-    :vartype description: Optional[LangString]
-    :ivar editorial_notes: A list of LangString objects containing editorial notes associated with the element.
-    :vartype editorial_notes: list[LangString]
-    :ivar creators: A list of URIs represented as strings identifying the creators of the element.
-    :vartype creators: list[str]
-    :ivar contributors: A list of URIs represented as strings identifying the contributors to the element.
-    :vartype contributors: list[str]
-    :cvar model_config: Configuration settings for the Pydantic model.
-    :vartype model_config: Dict[str, Any]
-    """
-
-    names: set[LangString] = Field(default_factory=set)
-    alt_names: set[LangString] = Field(default_factory=set)
-    description: Optional[LangString] = Field(default=None)
-    editorial_notes: set[LangString] = Field(default_factory=set)
-    creators: set[str] = Field(default_factory=set)  # Empty strings are not allowed in the set
-    contributors: set[str] = Field(default_factory=set)  # Empty strings are not allowed in the set
-
-    model_config = {
-        "arbitrary_types_allowed": True,
-        "validate_assignment": True,
-        "extra": "forbid",
-        "str_strip_whitespace": True,
-        "validate_default": True,
-    }
-
-    @field_validator("creators", "contributors", mode="after")
-    @classmethod
-    def __ensure_non_empty(cls, checked_values: set[str], checked_field: ValidationInfo) -> set[str]:
-        for elem in checked_values:
-            if elem == "":
-                error_message = format_error_message(
-                    description=f"Invalid empty string in {cls.__name__} list.",
-                    cause=f"Empty string found in '{cls.__name__}' field {checked_field.field_name}.",
-                    solution=f"Ensure all elements in the {checked_field.field_name} list are non-empty strings.",
-                )
-                raise ValueError(error_message)
-        return checked_values
-
-    @abstractmethod
-    def __init__(self, **data: dict[str, Any]) -> None:
-        """Initialize a new NamedElement instance, ensuring proper initialization of attributes with validation.
-
-        Inherits 'created', 'modified', and 'id' initialization from OntoumlElement, and adds initialization for
-        additional attributes specific to NamedElement. This class functions as the exclusive categorizer for the
-        subclasses 'Project' and 'ModelElement' within an OntoUML metamodel, ensuring that no instances of other
-        subclasses are created, maintaining the integrity of the generalization set.
-
-        :param data: Fields to be set on the model instance, including inherited and class-specific attributes.
-        :type data: dict[str, Any]
-        """
-
-        # List of allowed subclasses: NamedElement is a categorizer of a complete generalization set
-        self._validate_subclasses(["Diagram", "ModelElement", "Project"])
-
-        # Sets attributes
-        super().__init__(**data)
+    names: set[LangString] = field(default_factory=set)
+    alt_names: set[LangString] = field(default_factory=set)
+    description: Optional[LangString] = field(default=None)
+    editorial_notes: set[LangString] = field(default_factory=set)
+    creators: set[str] = field(default_factory=set)
+    contributors: set[str] = field(default_factory=set)
--- ontouml_py/model/ontoumlelement.py
+++ ontouml_py/model/ontoumlelement.py
@@ -1,120 +1,16 @@
-"""This module defines the core elements of an OntoUML model, providing a robust framework for representing and
-managing OntoUML elements. OntoUML is a well-established modeling language used primarily in the field of
-ontology-driven conceptual modeling.
-
-The module includes an abstract base class, `OntoumlElement`, which serves as the foundation for all other
-elements in an OntoUML model. This class encapsulates common features such as unique identification, creation
-and modification timestamps, and validation logic to ensure the integrity of these attributes.
-"""
 import uuid
 from abc import ABC
-from abc import abstractmethod
+from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Any
 from typing import Optional
 
-from pydantic import BaseModel
-from pydantic import Field
 
-from ontouml_py.utils.error_message import format_error_message
+@dataclass
+class OntoumlElement(ABC):
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
+    created: datetime = field(default_factory=datetime.now)
+    modified: Optional[datetime] = None
 
-
-class OntoumlElement(ABC, BaseModel):
-    """
-    Abstract base class representing a generic element within an OntoUML model.
-
-    This class provides foundational attributes and methods for all OntoUML elements, including unique identification
-    and timestamps for creation and modification. It enforces constraints on certain attributes and includes validation
-    logic to maintain their integrity.
-
-    :ivar id: A unique identifier for the element, automatically generated upon instantiation.
-    :vartype id: str
-    :ivar created: Timestamp when the element was created, defaults to the current time.
-    :vartype created: datetime
-    :ivar modified: Timestamp when the element was last modified, can be None if not modified.
-    :vartype modified: Optional[datetime]
-    :cvar model_config: Configuration settings for the Pydantic model.
-    :vartype model_config: Dict[str, Any]
-    """
-
-    id: str = Field(min_length=1, default_factory=lambda: str(uuid.uuid4()))
-    created: datetime = Field(default_factory=datetime.now)
-    modified: Optional[datetime] = Field(default=None)
-
-    model_config = {
-        "validate_assignment": True,
-        "validate_default": True,
-        "extra": "forbid",
-        "str_strip_whitespace": True,
-    }
-
-    @abstractmethod
-    def __init__(self, **data: dict[str, Any]) -> None:
-        """
-        Initialize a new OntoumlElement instance. This method is abstract and should be implemented by subclasses.
-
-        Ensures that 'modified' is not earlier than 'created'.
-
-        :param data: Fields to be set on the model instance.
-        :type data: dict[str, Any]
-        :raises ValueError: If 'modified' is set to a datetime earlier than 'created'.
-        """
-
-        # Sets attributes
-        super().__init__(**data)
-        self._validate_subclasses(["NamedElement", "Project", "ProjectElement", "Shape", "View"])
-
-    def __eq__(self, other: object) -> bool:
-        """
-        Determine if two OntoumlElement instances are equal based on their unique identifiers.
-
-        This method overrides the default equality comparison behavior. It is essential for comparing instances
-        of OntoumlElement, particularly in collections and when ensuring uniqueness. Equality is determined solely
-        based on the 'id' attribute, which is assumed to be a unique identifier for each instance.
-
-        :param other: The other object to compare with the current instance.
-        :type other: object
-        :return: True if both instances have the same 'id', False otherwise. Returns NotImplemented if 'other' is not
-                 an instance of OntoumlElement.
-        :rtype: bool
-        :raises TypeError: If 'other' is not an instance of OntoumlElement and cannot be compared.
-        """
-        if not isinstance(other, OntoumlElement):
-            return NotImplemented
-        return self.id == other.id
-
-    def __hash__(self) -> int:
-        """
-        Compute the hash value of an OntoumlElement instance based on its unique identifier.
-
-        This method enables OntoumlElement instances to be used in hash-based data structures like sets and dicts.
-        The hash value is computed using the 'id' attribute, ensuring that each instance has a distinct hash value
-        corresponding to its unique identifier.
-
-        :return: The hash value of the instance, computed using its 'id'.
-        :rtype: int
-        """
-        return hash(self.id)
-
-    @classmethod
-    def _validate_subclasses(cls, allowed_subclasses: list[str]) -> None:
-        """
-        Ensure that the given class is a subclass of one of the allowed subclasses.
-
-        :param allowed_subclasses: A list of allowed subclass names.
-        :type allowed_subclasses: list[str]
-        :raises ValueError: If the analyzed class is not a subclass of any allowed subclasses.
-        """
-        current_class = cls
-        while current_class != object:
-            if current_class.__name__ in allowed_subclasses:
-                return
-            current_class = current_class.__bases__[0]
-        else:
-            allowed = ", ".join(allowed_subclasses)
-            error_message = format_error_message(
-                description=f"Invalid subclass type for class '{cls.__name__}'.",
-                cause=f"'{cls.__name__}' is not an allowed subclass.",
-                solution=f"Use one of the allowed subclasses: {allowed}.",
-            )
-            raise ValueError(error_message)
+    def __post_init__(self):
+        if not self.id:
+            raise ValueError("The 'id' field must not be empty.")
