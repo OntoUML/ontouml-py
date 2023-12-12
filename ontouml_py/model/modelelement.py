631b44977369644be120684b341ea7b01a4f6df1
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-12-12 17:22:26 +0100

    pydantic removed from modelelement.py

--- ontouml_py/model/modelelement.py
+++ ontouml_py/model/modelelement.py
@@ -1,55 +1,11 @@
-"""This module defines the ModelElement class, which represents a model element in an OntoUML model. It inherits \
-properties from both NamedElement and ProjectElement, and includes additional features specific to model elements."""
-from abc import abstractmethod
+from dataclasses import dataclass, field
 from typing import Any
-
-from pydantic import Field
 
 from ontouml_py.model.namedelement import NamedElement
 from ontouml_py.model.projectelement import ProjectElement
 
 
+@dataclass
 class ModelElement(ProjectElement, NamedElement):
-    """Represents a model element, inheriting properties from both NamedElement and ProjectElement.
 
-    :ivar custom_properties: A set of custom properties associated with the model element. Each property is a tuple
-        containing a string key and a value of any type.
-    :vartype custom_properties: Set[Tuple[str, Any]]
-    :cvar model_config: Configuration settings for the Pydantic model.
-    :vartype model_config: Dict[str, Any]
-    """
-
-    custom_properties: set[tuple[str, Any]] = Field(default_factory=set)
-
-    model_config = {
-        "arbitrary_types_allowed": True,
-        "validate_assignment": True,
-        "extra": "forbid",
-        "str_strip_whitespace": True,
-        "validate_default": True,
-    }
-
-    @abstractmethod
-    def __init__(self, project, **data: dict[str, Any]) -> None:
-        """Initialize a new ModelElement instance.
-
-        :param data: Fields to be set on the model instance. This includes fields inherited from NamedElement and
-            ProjectElement, as well as any additional fields specific to ModelElement.
-        :type data: Dict[str, Any]
-        :raises ValueError: If the instance does not belong to the allowed subclasses.
-        """
-
-        NamedElement.__init__(self, **data)
-        ProjectElement.__init__(self, project, **data)
-        self._validate_subclasses(
-            [
-                "Decoratable",
-                "Generalization",
-                "GeneralizationSet",
-                "Anchor",
-                "Literal",
-                "Note",
-                "Package",
-                "Packageable",
-            ],
-        )
+    custom_properties: set[tuple[str, Any]] = field(default_factory=set)
--- tests/test_modelelement.py
+++ tests/test_modelelement.py
@@ -1,58 +1,22 @@
-from typing import Any
-
 import pytest
 
 from ontouml_py.model.modelelement import ModelElement
 
 
-class Anchor(ModelElement):
-    def __init__(self, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
+def test_modelelement_creation_with_default_values():
+    """Test the creation of a ModelElement with default values."""
+    element = ModelElement()
+    assert isinstance(element.custom_properties, set), "'custom_properties' should be a set"
 
+def test_modelelement_custom_properties_assignment():
+    """Test the assignment of custom properties in ModelElement."""
+    custom_properties = {("key1", "value1"), ("key2", 2)}
+    element = ModelElement(custom_properties=custom_properties)
+    assert element.custom_properties == custom_properties, "'custom_properties' should be assigned correctly"
 
-@pytest.fixture
-def sample_model_element() -> Anchor:
-    """Fixture to create a sample Anchor instance for testing.
-
-    :return: An instance of Anchor for testing purposes.
-    """
-    return Anchor()
-
-
-def test_model_element_initialization() -> None:
-    """Test the initialization of a ModelElement instance."""
-    element = Anchor(custom_properties={("key1", "value1"), ("key2", "value2")})
-    assert element.custom_properties == {
-        ("key1", "value1"),
-        ("key2", "value2"),
-    }, "ModelElement should initialize with the provided custom properties."
-
-
-def test_model_element_custom_properties_assignment(sample_model_element: Anchor) -> None:
-    """Test assigning custom properties to a ModelElement instance.
-
-    :param sample_model_element: A sample Anchor instance.
-    """
-    sample_model_element.custom_properties = {("key3", "value3")}
-    assert sample_model_element.custom_properties == {
-        ("key3", "value3")
-    }, "ModelElement should correctly assign new custom properties."
-
-
-def test_model_element_custom_properties_update(sample_model_element: Anchor) -> None:
-    """Test updating custom properties of a ModelElement instance.
-
-    :param sample_model_element: A sample Anchor instance.
-    """
-    sample_model_element.custom_properties.add(("key4", "value4"))
-    assert (
-        "key4",
-        "value4",
-    ) in sample_model_element.custom_properties, "ModelElement should allow updating custom properties."
-
-
-def test_model_element_invalid_custom_properties_type() -> None:
-    """Test assigning invalid type to custom properties of a ModelElement instance."""
-    with pytest.raises(ValueError):
-        Anchor(custom_properties="invalid_type")
-        pytest.fail("ModelElement should not accept non-set types for custom properties.")
+def test_modelelement_inherited_properties():
+    """Test that ModelElement correctly inherits properties from ProjectElement and NamedElement."""
+    element = ModelElement()
+    # Testing inherited properties from NamedElement and ProjectElement
+    assert hasattr(element, "id"), "ModelElement should inherit 'id' from OntoumlElement"
+    assert hasattr(element, "names"), "ModelElement should inherit 'names' from NamedElement"
